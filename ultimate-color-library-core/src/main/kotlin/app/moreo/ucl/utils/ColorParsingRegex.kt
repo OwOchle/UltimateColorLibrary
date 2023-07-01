6c0f00332cd777ff80ef515fe7f43bb4f8f1a632
MoreOwO <romeoboub@gmail.com>
2023-07-01 19:52:44 +0200

    Added rgba and hsla to rgb and hsl parsing

--- ultimate-color-library-core/src/main/kotlin/app/moreo/ucl/utils/ColorParsingRegex.kt
+++ ultimate-color-library-core/src/main/kotlin/app/moreo/ucl/utils/ColorParsingRegex.kt
@@ -2,8 +2,8 @@ package app.moreo.ucl.utils
 
 internal val SHORT_HEX_RGB = Regex("#(?<red>[0-9a-f])(?<green>[0-9a-f])(?<blue>[0-9a-f])(?<alpha>[0-9a-f])?\$", RegexOption.IGNORE_CASE)
 internal val LONG_HEX_RGB = Regex("#(?<red>[0-9a-f]{2})(?<green>[0-9a-f]{2})(?<blue>[0-9a-f]{2})(?<alpha>[0-9a-f]{2})?\$", RegexOption.IGNORE_CASE)
-internal val CSS_RGB = Regex("rgb\\( *(?<red>[0-9]{1,3}%?)[ ,]*(?<green>[0-9]{1,3}%?)[ ,]*(?<blue>[0-9]{1,3}%?) ?[,/]? ?(?<alpha>[0-9.]+%?)?\\)\$")
-internal val CSS_HSL = Regex("hsl\\( *(?<hue>[0-9]*(?:\\.[0-9])?)(?<hueUnit>deg|turn|rad|grad|)[ ,]*(?<saturation>[0-9]{1,3}%)[ ,]*(?<lightness>[0-9]{1,3}%)(?: ?[,/] ?(?<alpha>[0-9]*(?:\\.[0-9])?%?))?\\)\$")
+internal val CSS_RGB = Regex("rgba?\\( *(?<red>[0-9]{1,3}%?)[ ,]*(?<green>[0-9]{1,3}%?)[ ,]*(?<blue>[0-9]{1,3}%?) ?[,/]? ?(?<alpha>[0-9.]+%?)?\\)\$")
+internal val CSS_HSL = Regex("hsla?\\( *(?<hue>[0-9]*(?:\\.[0-9])?)(?<hueUnit>deg|turn|rad|grad|)[ ,]*(?<saturation>[0-9]{1,3}%)[ ,]*(?<lightness>[0-9]{1,3}%)(?: ?[,/] ?(?<alpha>[0-9]*(?:\\.[0-9])?%?))?\\)\$")
 
 internal enum class ParsedColorType {
     SHORT_HEX,
